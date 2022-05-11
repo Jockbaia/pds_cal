d68de4742510fe63e65672c7c223bfcb4ca4bc55
Aurora Anna Pia Sergio <s290363@studenti.polito.it>
2022-05-11 20:42:20 +0300

    add: basic network management

--- pds_cal.cpp
+++ pds_cal.cpp
@@ -1,15 +1,31 @@
 #include "pds_cal.h"
 #include "./ui_pds_cal.h"
+#include <QtNetwork/QNetworkAccessManager>
+#include <QtNetwork/QNetworkRequest>
+#include <QtNetwork/QNetworkReply>
+
 
 pds_cal::pds_cal(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::pds_cal)
 {
     ui->setupUi(this);
+    mManager = new QNetworkAccessManager(this);
+    connect(mManager, &QNetworkAccessManager::finished, this, [&](QNetworkReply *reply){
+        QByteArray data = reply->readAll();
+        QString str = QString::fromLatin1(data);
+        ui->plainTextEdit->setPlainText(str);
+    });
 }
 
 pds_cal::~pds_cal()
 {
     delete ui;
+}
+
+
+void pds_cal::on_getButton_clicked()
+{
+    mManager->get(QNetworkRequest(QUrl(ui->urlLineEdit->text())));
 }
 
--- pds_cal.h
+++ pds_cal.h
@@ -2,10 +2,13 @@
 #define PDS_CAL_H
 
 #include <QMainWindow>
+#include <QtNetwork/QNetworkAccessManager>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class pds_cal; }
 QT_END_NAMESPACE
+
+class QNetworkAccessManager;
 
 class pds_cal : public QMainWindow
 {
@@ -15,7 +18,11 @@ public:
     pds_cal(QWidget *parent = nullptr);
     ~pds_cal();
 
+private slots:
+    void on_getButton_clicked();
+
 private:
     Ui::pds_cal *ui;
+    QNetworkAccessManager *mManager;
 };
 #endif // PDS_CAL_H
